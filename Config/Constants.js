cce6f57efbd427b10f0cf08bf6b5f1f4cc171d0d
Josh Perry <josh.perry@roostermornings.com>
2019-07-18 13:31:18 +0800

    Fixed syntax errors

--- Config/Constants.js
+++ Config/Constants.js
@@ -9,11 +9,11 @@ const PROD = 1;
 const ENV = PROD;
 
 // TODO: move to env vars
-const envSpecificConfig = {};
+let envSpecificConfig = {};
 if (ENV === DEV) {
     envSpecificConfig = {
         API_KEY: "878ed9d6-8729-4883-a93f-2ceb10b643ce",
-        AWS_CRED_FILENAME = "aws-credentials.json", 
+        AWS_CRED_FILENAME: "aws-credentials.json", 
 
         // S3 Buckets
         BUCKET_FIRMWARE_BINARIES: "sensesir-firmware-releases",
@@ -21,7 +21,7 @@ if (ENV === DEV) {
 } else if (ENV === PROD) {
     envSpecificConfig = {
         API_KEY: "31f76620-3e10-4290-b3d8-2be6f4956484",
-        AWS_CRED_FILENAME = "aws-credentials-prod.json",
+        AWS_CRED_FILENAME: "aws-credentials-prod.json",
 
         // S3 Buckets
         BUCKET_FIRMWARE_BINARIES: "sensesir-firmware-releases-prod",
